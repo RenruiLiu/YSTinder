a0e53e79488b1da595ffec86f472818ece583209
RenruiLiu <liurenrui016@qq.com>
2019-01-09 09:23:20 +0800

    Initial Commit

---
+++ YSTinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YSTinder
+//
+//  Created by jozen on 2019/1/9.
+//  Copyright © 2019 liurenrui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
